e398186c12ed4abde028fb70f417f48c83f09fd1
Yash Haldiya <yashasvi9199@gmail.com>
2024-01-06 23:59:12 +0530

    added images for user page

--- client/src/components/user-pages/User.jsx
+++ client/src/components/user-pages/User.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import './User.css'
+import name_icon from '../assets/svg icons/name.svg'
+import user_icon from '../assets/svg icons/user.svg'
+import email_icon from '../assets/svg icons/at.svg'
+import lock_icon from '../assets/svg icons/lock.svg'
+import unlock_icon from '../assets/svg icons/unlock.svg'
+import show_icon from '../assets/svg icons/showpwd.svg'
+import hide_icon from '../assets/svg icons/hidepwd.svg'
+
 
 function User() {
   return (
@@ -9,28 +17,29 @@ function User() {
                     <form className="signin left">
                         <p id="heading">Register</p>
                         <div className="field">
-                            <img />
-                            <input placeholder="First Name" className="input-field" type="text" />
+                            <img src={name_icon} alt="icon" className="input-icon" height="16" width="16" />
+                            <input placeholder="Full Name" className="input-field" type="text" />
                         </div>
                         <div className="field">
-                            <img />
-                            <input placeholder="Last Name" className="input-field" type="text" />
-                        </div>
-                        <div className="field">
-                            <img />
+                            <img src={user_icon} alt="icon" className="input-icon" height="16" width="16" />
                             <input placeholder="Username" className="input-field" type="text" />
                         </div>
                         <div className="field">
-                            <img />
+                            <img src={email_icon} alt="icon" className="input-icon" height="16" width="16" />
                             <input placeholder="Email" className="input-field" type="text" />
                         </div>
                         <div className="field">
-                            <img />
+                            <img src={lock_icon} alt="icon" className="input-icon" height="16" width="16" />
                             <input placeholder="Password" className="input-field" type="password" />
+                            <img src={show_icon} alt="icon" className="input-icon" height="16" width="16" />
+                            <img src={hide_icon} alt="icon" className="input-icon" height="16" width="16" hidden/>
+                            
                         </div>
                         <div className="field">
-                            <img />
+                            <img src={lock_icon} alt="icon" className="input-icon" height="16" width="16" />
                             <input placeholder="Confirm Password" className="input-field" type="password" />
+                            <img src={show_icon} alt="icon" className="input-icon" height="16" width="16" />
+                            <img src={hide_icon} alt="icon" className="input-icon" height="16" width="16" hidden/>
                         </div>
                         <div className="btn">
                             <button className="button1">Submit</button>&nbsp;&nbsp;&nbsp;&nbsp;
@@ -47,12 +56,14 @@ function User() {
                 <form className="login right">
                     <p id="heading">Login</p>
                     <div className="field">
-                        <img src="at.svg" alt="icon" className="input-icon" height="20" width="20" />
-                        <input autoComplete="off" placeholder="Username" className="input-field" type="text" />
+                        <img src={user_icon} alt="icon" className="input-icon" height="20" width="20" />
+                        <input autoComplete="off" placeholder="Username/ Email" className="input-field" type="text" />
                     </div>
                     <div className="field">
-                        <img src="pass.svg" alt="icon" className="input-icon" height="16" width="16" />
+                        <img src={lock_icon} alt="icon" className="input-icon" height="16" width="16" />
                         <input placeholder="Password" className="input-field" type="password" />
+                        <img src={show_icon} alt="icon" className="input-icon" height="16" width="16" />
+                        <img src={hide_icon} alt="icon" className="input-icon" height="16" width="16" hidden/>
                     </div>
                     <div className="btn">
                         <button className="button1">Login</button>
